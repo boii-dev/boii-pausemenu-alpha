888b2e02def996236b6d4c61c40e28dfc33ef46a
AbstractBunnyy <167042875+AbstractBunnyy@users.noreply.github.com>
2024-04-29 19:33:13 -0400

    optimized code and fixed exploits
    Signed-off-by: AbstractBunnyy <167042875+AbstractBunnyy@users.noreply.github.com>

--- server/pausemenu_sv.lua
+++ server/pausemenu_sv.lua
@@ -1,48 +1,57 @@
-----------------------------------
 --<!>-- BOII | DEVELOPMENT --<!>--
 ----------------------------------
 
--- Event to collect (data) from nui callback
+-- Event to collect data from NUI callback and send a report
 RegisterNetEvent('boii-pausemenu:SendReport')
 AddEventHandler('boii-pausemenu:SendReport', function(data)
-	local ids = ExtractIdentifiers(source)
-	local NewReport = {
-		playerid = source,
-		fname = data.fname,
+    local identifiers = ExtractIdentifiers(source)
+    local NewReport = {
+        playerid = source,
+        fname = data.fname,
         lname = data.lname,
         reporttype = data.reporttype,
-		subject = data.subject,
-		description = data.description,
-		discord = '<@'..ids.discord:gsub('discord:', '')..'>',
-        license = ids.license:gsub('license2:', '')
-	}
-	SendWebhook(NewReport)
+        subject = data.subject,
+        description = data.description,
+        discord = '<@' .. identifiers.discord:gsub('discord:', '') .. '>',
+        license = identifiers.license:gsub('license2:', '')
+    }
+    SendWebhook(NewReport)
 end)
--- Function to send (data) through webhook
+
+-- Function to send data through webhook
 function SendWebhook(data)
-    local footertext = Config.Discord.BotData.Footer.Text
+    local footerText = Config.Discord.BotData.Footer.Text
     local icon = Config.Discord.BotData.Footer.Icon
-    local botname = Config.Discord.BotData.Name
-    local botlogo = Config.Discord.BotData.Logo
-	local report = {
-		{
-            ['title'] = '**Type:** '..data.reporttype,
-			['color'] = Config.Discord.Colour,
-			['footer'] = {
-                ['text'] = footertext..os.date('%c'),
-                ['icon_url'] = icon,
+    local botName = Config.Discord.BotData.Name
+    local botLogo = Config.Discord.BotData.Logo
+
+    local report = {
+        {
+            title = '**Type:** ' .. data.reporttype,
+            color = Config.Discord.Colour,
+            footer = {
+                text = footerText .. os.date('%c'),
+                icon_url = icon,
             },
-            ['description'] = '**First name:** '..data.fname..'\n**Last name:** '..data.lname..'\n**Subject:** '..data.subject..'\n**Description:** '..data.description..'\n**ID:** '..data.playerid..'\n**Discord:** '..data.discord..'\n**License:** ||'..data.license..'||',
-		}
-	}
-	PerformHttpRequest(Config.Discord.Webhook, function(err, text, headers) end, 'POST', json.encode({username = botname, embeds = report, avatar_url = botlogo}), {['Content-Type'] = 'application/json'})
+            description = string.format('**First name:** %s\n**Last name:** %s\n**Subject:** %s\n**Description:** %s\n**ID:** %s\n**Discord:** %s\n**License:** ||%s||',
+                data.fname, data.lname, data.subject, data.description, data.playerid, data.discord, data.license)
+        }
+    }
+
+    PerformHttpRequest(Config.Discord.Webhook, function(err, text, headers) end, 'POST', json.encode({
+        username = botName,
+        embeds = report,
+        avatar_url = botLogo
+    }), { ['Content-Type'] = 'application/json' })
 end
+
 -- Event to drop player
 RegisterServerEvent('boii-pausemenu:DropPlayer')
 AddEventHandler('boii-pausemenu:DropPlayer', function()
-	DropPlayer(source, 'You disconnected from the server.')
+    DropPlayer(source, 'You disconnected from the server.')
 end)
--- Function to grab indentifiers; borrowed from BADGER-ANTICHEAT <3
+
+-- Function to grab identifiers
 function ExtractIdentifiers(id)
     local identifiers = {
         steam = '',
@@ -52,6 +61,7 @@ function ExtractIdentifiers(id)
         xbl = '',
         live = ''
     }
+
     for i = 0, GetNumPlayerIdentifiers(id) - 1 do
         local playerID = GetPlayerIdentifier(id, i)
         if string.find(playerID, 'steam') then
@@ -68,5 +78,6 @@ function ExtractIdentifiers(id)
             identifiers.live = playerID
         end
     end
+
     return identifiers
 end
