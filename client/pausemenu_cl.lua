471f71676c157e9a0e990bf20acd337ec1396e4b
AbstractBunnyy <167042875+AbstractBunnyy@users.noreply.github.com>
2024-04-29 19:32:28 -0400

    Improved code and fixed exploits
    Signed-off-by: AbstractBunnyy <167042875+AbstractBunnyy@users.noreply.github.com>

--- client/pausemenu_cl.lua
+++ client/pausemenu_cl.lua
@@ -1,45 +1,53 @@
-----------------------------------
 --<!>-- BOII | DEVELOPMENT --<!>--
 ----------------------------------
 
+local mapOpen = false
+
 -- Thread to trigger menu on keypress
 Citizen.CreateThread(function()
-	while true do
-	    Wait(1)
-		SetPauseMenuActive(false)
-	    if (IsControlJustPressed(0, 200)) then
-            TransitionToBlurred(1000)
-            SetNuiFocus(true, true)
-            SendNUIMessage({
-            open = true
-            })
-		end
-	end
+    while true do
+        Wait(1)
+        SetPauseMenuActive(false)
+        if IsControlJustPressed(0, 200) then
+            if not IsPauseMenuActive() then
+                if mapOpen then
+                    CloseMap()
+                    mapOpen = false
+                else
+                    TriggerEvent("boii-pausemenu:OpenMenu")
+                end
+            end
+        end
+    end
 end)
--- Function to close menu
-function ClosePause()
-	TransitionFromBlurred(1000)
-    SetNuiFocus(false, false)
-    SendNUIMessage({
-        open = false
-    })
-end
+
+-- Event to open the menu
+RegisterNetEvent("boii-pausemenu:OpenMenu")
+AddEventHandler("boii-pausemenu:OpenMenu", function()
+    TransitionToBlurred(1000)
+    SetNuiFocus(true, true)
+    SendNUIMessage({ open = true })
+end)
+
 -- Close button; closes menu
 RegisterNUICallback('Close', function(data)
     ClosePause()
 end)
+
 -- Settings button; sends to GTA options
 RegisterNUICallback('Settings', function(data)
-	ClosePause()
-	ActivateFrontendMenu(GetHashKey('FE_MENU_VERSION_MP_PAUSE'),0,-1)
-end)
--- Disconnect button; drops player
-RegisterNUICallback('DropPlayer', function(data)
-    TriggerServerEvent('boii-pausemenu:DropPlayer')
-end)
--- Submit button; submits a report as a discord webhook
-RegisterNUICallback('NewReport', function(data)
-    local NewReport = {fname = data.fname, lname = data.lname, reporttype = data.reporttype, subject = data.subject, description = data.description}
-    TriggerServerEvent('boii-pausemenu:SendReport', NewReport)
+    ClosePause()
+    ActivateFrontendMenu(GetHashKey('FE_MENU_VERSION_MP_PAUSE'), 0, -1)
 end)
 
+-- Function to close menu
+function ClosePause()
+    TransitionFromBlurred(1000)
+    SetNuiFocus(false, false)
+    SendNUIMessage({ open = false })
+end
+
+-- Event to listen for map opening
+RegisterCommand("map", function()
+    mapOpen = not mapOpen
+end)
